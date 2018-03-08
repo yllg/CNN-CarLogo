cc29ec766d128671fc5d3ad70165e072fdfbd5ef
yllg <1565550208@qq.com>
2018-03-08 22:30:22 +0800

    CNN卷积神经网络实现汽车logo的图像识别

---
+++ car.js
@@ -0,0 +1,32 @@
+var carList = [
+    {
+        "name":"奥迪",
+        "url":"http://p.pstatp.com/avatar/100x100/1dd5000048d6334c26b4.png",
+        "index":0
+    },
+    {
+        "name":"阿尔法-罗密欧",
+        "url":"http://p.pstatp.com/avatar/100x100/1dd500003399ce089bef.png",
+        "index":1
+    },
+    {
+        "name":"阿斯顿 马丁",
+        "url":"http://p.pstatp.com/avatar/100x100/1dd5000018f89ff63f66.png",
+        "index":2
+    },
+    {
+        "name":"AC Schnitzer",
+        "url":"http://p.pstatp.com/avatar/100x100/2f1e00004430bdcb8ae4.png",
+        "index":3
+    },
+    {
+        "name":"ALPINA",
+        "url":"http://p.pstatp.com/avatar/100x100/1dd50000198aee2e4688.png",
+        "index":4
+    },
+    {
+        "name":"安凯客车",
+        "url":"http://p.pstatp.com/avatar/100x100/2f1e000103379037dc9f.png",
+        "index":5
+    }
+]
